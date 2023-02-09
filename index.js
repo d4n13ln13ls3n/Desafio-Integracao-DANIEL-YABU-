a8fd5c7ef4c035840312ad897fa95dc1ec409480
Daniel Yabu <danielyabu@yahoo.com>
2023-02-09 20:46:20 -0300

    Feat: app working

--- index.js
+++ index.js
@@ -1,83 +1,76 @@
-import fs from 'fs'
-import path from 'path'
-import process from 'process'
-import { authenticate } from '@google-cloud/local-auth'
 import { google } from 'googleapis'
+import hubspot from '@hubspot/api-client'
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-const TOKEN_PATH = path.join(process.cwd(), 'token.json')
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json')
+const sheets = google.sheets({
+  version: 'v4',
+  auth: process.env.GOOGLE_API_KEY
+})
 
-/**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
- */
-async function loadSavedCredentialsIfExist () {
+const hubspotClient = new hubspot.Client({ accessToken: process.env.HUBSPOT_API_KEY })
+
+async function listContactsFromGoogleSheets (spreadsheetId, sheetName) {
   try {
-    const content = await fs.readFile(TOKEN_PATH)
-    const credentials = JSON.parse(content)
-    return google.auth.fromJSON(credentials)
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range: [sheetName]
+    })
+
+    return response.data.values
   } catch (err) {
-    return null
+    console.error(err)
   }
 }
 
-/**
- * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials (client) {
-  const content = await fs.readFile(CREDENTIALS_PATH)
-  const keys = JSON.parse(content)
-  const key = keys.installed || keys.web
-  const payload = JSON.stringify({
-    type: 'authorized_user',
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token
-  })
-  await fs.writeFile(TOKEN_PATH, payload)
+function parseSheetValues (values) {
+  const headers = ['company', 'firstname', 'email', 'phone', 'website']
+  const valuesWithoutHeaders = values.slice(1)
+  return valuesWithoutHeaders.map((value) =>
+    headers.reduce(
+      (acc, header, index) => ({ ...acc, [header]: value[index] }),
+      {}
+    )
+  )
 }
 
-async function authorize () {
-  let client = await loadSavedCredentialsIfExist()
-  if (client) {
-    return client
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH
+async function postContactsFromGoogleSheets (contacts) {
+  contacts.forEach((contact) => {
+    const index = contact.email.indexOf('@')
+    const emailDomain = contact.email
+      .slice(index + 1, contact.email.length)
+      .split('.')[0]
+    const commercialDomains = ['yahoo', 'hotmail', 'gmail', 'apple', 'outlook']
+    const isCommercial = commercialDomains.some(
+      (domain) => domain === emailDomain
+    )
+    if (isCommercial) {
+      throw new Error('Email must be corporate')
+    }
   })
-  if (client.credentials) {
-    await saveCredentials(client)
+
+  try {
+    const inputs = contacts.map((contact) => ({ properties: contact }))
+    await hubspotClient.crm.contacts.batchApi.create({ inputs })
+  } catch (e) {
+    e.message === 'HTTP request failed'
+      ? console.error(JSON.stringify(e.response, null, 2))
+      : console.error(e)
   }
-  return client
 }
 
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
-async function listMajors (auth) {
-  const sheets = google.sheets({ version: 'v4', auth })
-  const res = await sheets.spreadsheets.values.get({
-    spreadsheetId: '1lWfM4N7EXb6wyZ6IR0bsXftS_iv3fqA18Ea0dEcrHDg',
-    range: 'Class customers!A2:E'
-  })
-  const rows = res.data.values
-  if (!rows || rows.length === 0) {
-    console.log('No data found.')
-    return
+async function run () {
+  const [spreadsheetId, sheetName] = process.argv.slice(2)
+  if (!spreadsheetId) {
+    throw new Error('Missing required argument spreadsheetId')
   }
-  console.log('Name, Major:')
-  rows.forEach((row) => {
-    console.log(`${row[0]}, ${row[4]}`)
-  })
+  if (!sheetName) {
+    throw new Error('Missing required argument sheetname')
+  }
+  const contacts = await listContactsFromGoogleSheets(
+    spreadsheetId, sheetName
+  )
+  const parsedContacts = parseSheetValues(contacts)
+
+  await postContactsFromGoogleSheets(parsedContacts)
 }
 
-authorize().then(listMajors).catch(console.error)
+run()
